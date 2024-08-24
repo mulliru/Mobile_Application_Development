4aab8798bdf5939832b66fb57a95ff78c0a19af0
Murillo Ferreira <murilloferreira348@gmail.com>
2024-08-24 15:50:56 -0300

    Adicionando a aula

---
+++ Primeiro_Semestre/Aula_05/ModificadoresDeAcesso/src/Main.kt
@@ -0,0 +1,25 @@
+
+open class Pessoa{
+    //Atributos, Propriedades
+    var nome:String = "Fernando"
+    //Metodos da Pessoa
+    fun andar(){
+        println("${nome} andando...")
+    }
+}
+
+
+
+
+
+    class Aluno:Pessoa(){}
+    //Propriedades, atributos
+    var matricula:Int
+
+
+
+
+fun main() {
+    println(3550<=10)
+
+}
